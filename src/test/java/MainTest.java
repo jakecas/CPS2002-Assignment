37e51ea1a46b4ec2adf6d9a981bb35b56d588ded
Jake Cassar <jake.cass10@gmail.com>
2019-04-10 20:44:34 +0200

    Added failing subtract test.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.*;
 public class MainTest {
 
     @Test
-    public void add_twoNumbers() {
-        assertEquals("Testing add method with 5+3.", 8, Main.add(5,3), 0);
+    public void add_twoNumbers_shouldBeAdded() {
+        assertEquals("Testing add method with 5+3=8.", 8, Main.add(5,3), 0);
+    }
+
+    @Test
+    public void subtract_twoNumbers_failingTest() {
+        assertEquals("Testing subtract method with 5-3=8.", 8, Main.subtract(5,3), 0);
     }
 }
