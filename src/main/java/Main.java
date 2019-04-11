64b58e4a95467f1a12098c4821ad93da5e7521fb
Jake Cassar <jake.cass10@gmail.com>
2019-04-11 16:42:31 +0200

    Refactored fake test methods to Calculator.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,4 @@ public class Main {
         System.out.println("Hello World!");
     }
 
-    public static double add(double x, double y){
-        return x+y;
-    }
-
-    public static double subtract(double x, double y){
-        return x-y;
-    }
-
-    public static double multiply(double x, double y){
-        return x*y;
-    }
-
-    public static double divide(double x, double y){
-        return x/y;
-    }
 }
